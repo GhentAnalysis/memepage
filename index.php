7b5d0c019e7cb469825d853d8c2c7a1e1257a81f
Joscha Knolle <joscha.knolle@cern.ch>
2024-01-22 14:39:31 +0100

    Switched to search for user contributions in php file

--- index.php
+++ index.php
@@ -1,41 +1,44 @@
 <?php
-// get and sort the file names with image extensions
-$pngnames = glob('*.png');
-$jpgnames = glob('*.jpg');
-$imgnames = array_merge($pngnames, $jpgnames);
-sort($imgnames);
-// select a random image
-$imgname = $imgnames[array_rand($imgnames)];
-// do the same for the background images
-$bkgnames = glob('style/*.jpg');
-sort($bkgnames);
-$bkgname = $bkgnames[array_rand($bkgnames)];
+    // get and sort the file names with image extensions
+    $files = glob('../../../*/public_html/memes/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+    $images = [];
+    foreach($files as $file) {
+        $components = explode("/", $file);
+        $images[] = "../../~".$components[3]."/memes/".$components[6];
+    }
+    sort($images);
+    // select a random image
+    $imgname = $images[array_rand($images)];
+    // do the same for the background images
+    $bkgnames = glob('style/*.jpg');
+    sort($bkgnames);
+    $bkgname = $bkgnames[array_rand($bkgnames)];
 ?>
 <html>
 <head>
 <!-- force refresh with given time interval -->
 <meta http-equiv="refresh" content="30">
 <!-- CSS style definitions -->
-<style type='text/css'>
-html {
-    min-height:100%;
-}
-body {
-    width:100%;
-    min-height:100%;
-    background:url(<?php print $bkgname; ?>);
-    background-size:cover;
-    margin:0px;
-}
-#meme {
-    margin:1%;
-    width:97%;
-    height:97%;
-    background:url(<?php print str_replace(" ", "%20", $imgname); ?>);
-    background-size:contain;
-    background-repeat:no-repeat;
-    background-position:center;
-}
+    <style type='text/css'>
+    html {
+        min-height:100%;
+    }
+    body {
+        width:100%;
+        min-height:100%;
+        background:url(<?php print $bkgname; ?>);
+        background-size:cover;
+        margin:0px;
+    }
+    #meme {
+        margin:1%;
+        width:97%;
+        height:97%;
+        background:url(<?php print str_replace(" ", "%20", $imgname); ?>);
+        background-size:contain;
+        background-repeat:no-repeat;
+        background-position:center;
+    }
 </style>
 </head>
 <body>
